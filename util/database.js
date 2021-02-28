10bb9cd537e7708a6491814ba67164e675a52009
Daniel_shiloah <daniel.s@helloeko.com>
2021-02-28 20:05:44 +0200

    initialized mongoDb as database use

--- util/database.js
+++ util/database.js
@@ -1,10 +1,28 @@
-const { Sequelize } = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('new_schema', 'root', 'ds170283', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+let _db;
 
 
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Daniel_shiloah:8oOBdwanxrWVyFjv@cluster0.86kjd.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+        console.log('Connected!');
+        _db = client.db();
+        callback();
+    })
+    .catch(err => {
+        console.log(err)
+        throw err;
+    });
+}
 
-module.exports = sequelize;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
