224040e8d6743c62a3860584eeb23ccb5aca6fd8
Daniel_shiloah <daniel.s@helloeko.com>
2021-03-11 15:29:00 +0200

    cookies, sessions and login

--- app.js
+++ app.js
@@ -3,53 +3,74 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const app = express();
+const mongoPw = process.env.TESTING_MONGODBPW;
+const MONGODB_URI = `mongodb+srv://Daniel_shiloah:${mongoPw}@cluster0.86kjd.mongodb.net/shop?retryWrites=true&w=majority`;
 
+const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+const { nextTick } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({ 
+        secret: 'my secret', 
+        resave: false, 
+        saveUninitialized: false, 
+        store: store 
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('603f9ebac60e9a3d58535c77')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => { console.log(err) });
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-const mongoPw = process.env.TESTING_MONGODBPW;
 mongoose
-.connect(`mongodb+srv://Daniel_shiloah:${mongoPw}@cluster0.86kjd.mongodb.net/shop?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => {
-    User.findOne().then(user => {
-        if (!user) {
-            const user = new User({
-                name: 'Daniel',
-                email: 'dan@test.com',
-                cart: {
-                    items: []
-                }
-            });
-            user.save();
-        }
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Daniel',
+                    email: 'dan@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
+        console.log('connected!');
+        app.listen(3000);
     })
-    console.log('connected!');
-    app.listen(3000);
-})
-.catch(err => console.log('Server Error',err));
+    .catch(err => console.log('Server Error', err));
---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    console.log(req.session.isLoggedIn);
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: req.session.isLoggedIn
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('603f9ebac60e9a3d58535c77')
+        .then(user => {
+            req.session.user = user;
+            req.session.isLoggedIn = true;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => { console.log(err) });
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
